616ff17c773f6f1b8b2dffb0704f0a65228fca85
buyaramadhan <ytchochote@gmail.com>
2024-05-04 22:43:03 +0700

    commit 16

--- 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
+++ 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
@@ -172,4 +172,18 @@ void searchData()
 	{
 		cout << "\nList is empty" << endl;
 	}
-	
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search:";
+	int num;
+	cin >> num;
+	if (search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else
+	{
+		cout << "\nRecord found" << endl;
+		cout << "\mRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
+}
+
