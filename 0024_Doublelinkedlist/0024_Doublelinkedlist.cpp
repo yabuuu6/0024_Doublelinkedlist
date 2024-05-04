9f3cdcd5365bde8c6e8685e4889bddde751a4921
buyaramadhan <ytchochote@gmail.com>
2024-05-04 22:40:18 +0700

    commit 8

--- 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
+++ 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
@@ -105,4 +105,23 @@ void deleteNode()
 		return;
 	}
 
-	
+	if (current == START)
+	{
+		START = START->next;
+		if (START != NULL)
+		{
+			START->prev = NULL;
+		}
+	}
+	else
+	{
+		previous->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = previous;
+		}
+	}
+
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << " delete/x1b[0m" << endl;
+
