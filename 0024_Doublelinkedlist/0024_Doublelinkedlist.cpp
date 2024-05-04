45eb1a9781fa2d22d3666357137b7fd153c3ccab
buyaramadhan <ytchochote@gmail.com>
2024-05-04 22:39:33 +0700

    commit 6

--- 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
+++ 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
@@ -76,3 +76,18 @@ bool search(int rollNo, Node **previous, Node **current)
 	return (*current != NULL);
 }
 
+void deleteNode()
+{
+	Node* previous, * current;
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
+
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
+
+	
