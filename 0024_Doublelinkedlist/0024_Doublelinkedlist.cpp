5744d7b50dc4510dd143b947c95ab04012caddb3
buyaramadhan <ytchochote@gmail.com>
2024-05-04 22:42:34 +0700

    commit 14

--- 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
+++ 0024_Doublelinkedlist/0024_Doublelinkedlist.cpp
@@ -152,4 +152,17 @@ void revtraverse()
 	if (listEmpty())
 		cout << "\nList is empty" << endl;
 	else
-	
+	{
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
